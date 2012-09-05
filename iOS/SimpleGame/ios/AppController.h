a835cd22aeab026eeccefb742b142a158f6f62ca
Gonzalo Diaz Cruz <gonzalo@studiopangea.com>
2012-09-05 00:39:16 -0300

    Initial hybrid project architecture

---
+++ iOS/SimpleGame/ios/AppController.h
@@ -0,0 +1,17 @@
+//
+//  SimpleGameAppController.h
+//  SimpleGame
+//
+//  Created by Gonzalo Diaz Cruz on 04-09-12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@end
+
