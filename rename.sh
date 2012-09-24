669aa089e15d7e162f3da75dbbf8f87caffbb354
Gonzalo Diaz Cruz <gonzalo@studiopangea.com>
2012-09-24 17:15:57 -0300

    iOS project renamed to Box2DSample

---
+++ rename.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ ! "$1" -o ! "$2" ] ; then
+    echo "Usage: $0 <Original Name> <New Name>";
+    exit
+fi
+
+ORIGNAME=$1
+NEWNAME=$2
+
+echo Replacing "<$ORIGNAME>" with "<$NEWNAME>"
+
+## RENAME FILES
+find . -depth -name "$ORIGNAME*" | while read file; do 
+	newfile=$(echo "$file" | sed s/"$ORIGNAME"/"$NEWNAME"/)
+	mv "$file" "$newfile";
+done
+
+## Search And Replace in files
+
+find . -not -iwholename '*.git*' -type f -print -exec file {} \; | grep text | cut -d ':' -f1 | while read file; do
+	echo $file
+	sed -i.bak s/"$ORIGNAME"/"$NEWNAME"/g $file #>/dev/null 2>&1
+	rm $file.bak >/dev/null 2>&1
+done
