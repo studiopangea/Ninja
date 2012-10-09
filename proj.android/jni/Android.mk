7707cc8727376c42115d4bfbfa11d149bae1dd10
Gonzalo Diaz Cruz <gonzalo@studiopangea.com>
2012-10-09 12:48:18 -0300

    Android project migrated to cocos2d-x 2.0.3

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -13,12 +13,11 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
                    
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes                   
 
-LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static cocosdenshion_static cocos_extension_static box2d_static
+LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static cocosdenshion_static cocos_extension_static
             
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,CocosDenshion/android) \
 $(call import-module,cocos2dx) \
-$(call import-module,extensions) \
-$(call import-module,external/Box2D)
+$(call import-module,extensions)
 
--- proj.android/src/org/cocos2dx/lib/Cocos2dxActivity.java
+++ proj.android/src/org/cocos2dx/lib/Cocos2dxActivity.java
@@ -20,244 +20,130 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
-****************************************************************************/
-
+ ****************************************************************************/
 package org.cocos2dx.lib;
 
+import org.cocos2dx.lib.Cocos2dxHelper.Cocos2dxHelperListener;
+
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.content.res.AssetManager;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.Message;
-import android.util.DisplayMetrics;
-import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
-public class Cocos2dxActivity extends Activity{
-    private static Cocos2dxMusic backgroundMusicPlayer;
-    private static Cocos2dxSound soundPlayer;
-    private static Cocos2dxAccelerometer accelerometer;
-    private static AssetManager assetManager;
-    private static boolean accelerometerEnabled = false;
-    private static Handler handler;
-    private final static int HANDLER_SHOW_DIALOG = 1;
-    private static String packageName;
+public abstract class Cocos2dxActivity extends Activity implements Cocos2dxHelperListener {
+	// ===========================================================
+	// Constants
+	// ===========================================================
 
-    private static native void nativeSetPaths(String apkPath);
+	@SuppressWarnings("unused")
+	private static final String TAG = Cocos2dxActivity.class.getSimpleName();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        // get frame size
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
-        accelerometer = new Cocos2dxAccelerometer(this);
-
-        // init media player and sound player
-        backgroundMusicPlayer = new Cocos2dxMusic(this);
-        soundPlayer = new Cocos2dxSound(this);
-        
-        // init asset manager for jni call
-        assetManager = getAssets();
-        
-        // init bitmap context
-        Cocos2dxBitmap.setContext(this);
-        
-        handler = new Handler(){
-        	public void handleMessage(Message msg){
-        		switch(msg.what){
-        		case HANDLER_SHOW_DIALOG:
-        			showDialog(((DialogMessage)msg.obj).title, ((DialogMessage)msg.obj).message);
-        			break;
-        		}
-        	}
-        };
-    }
-    
-    
-    public static AssetManager getAssetManager() {
-    	return assetManager;
-    }
-    
-    public static String getCurrentLanguage() {
-    	String languageName = java.util.Locale.getDefault().getLanguage();
-    	return languageName;
-    }
-    
-    public static void showMessageBox(String title, String message){
-    	Message msg = new Message();
-    	msg.what = HANDLER_SHOW_DIALOG;
-    	msg.obj = new DialogMessage(title, message);
-    	
-    	handler.sendMessage(msg);
-    } 
-
-    public static void enableAccelerometer() {
-        accelerometerEnabled = true;
-        accelerometer.enable();
-    }
-
-    public static void disableAccelerometer() {
-        accelerometerEnabled = false;
-        accelerometer.disable();
-    }
-
-    public static void preloadBackgroundMusic(String path){
-    	backgroundMusicPlayer.preloadBackgroundMusic(path);
-    }
-    
-    public static void playBackgroundMusic(String path, boolean isLoop){
-    	backgroundMusicPlayer.playBackgroundMusic(path, isLoop);
-    }
-    
-    public static void stopBackgroundMusic(){
-    	backgroundMusicPlayer.stopBackgroundMusic();
-    }
-    
-    public static void pauseBackgroundMusic(){
-    	backgroundMusicPlayer.pauseBackgroundMusic();
-    }
-    
-    public static void resumeBackgroundMusic(){
-    	backgroundMusicPlayer.resumeBackgroundMusic();
-    }
-    
-    public static void rewindBackgroundMusic(){
-    	backgroundMusicPlayer.rewindBackgroundMusic();
-    }
-    
-    public static boolean isBackgroundMusicPlaying(){
-    	return backgroundMusicPlayer.isBackgroundMusicPlaying();
-    }
-    
-    public static float getBackgroundMusicVolume(){
-    	return backgroundMusicPlayer.getBackgroundVolume();
-    }
-    
-    public static void setBackgroundMusicVolume(float volume){
-    	backgroundMusicPlayer.setBackgroundVolume(volume);
-    }
-    
-    public static int playEffect(String path, boolean isLoop){
-    	return soundPlayer.playEffect(path, isLoop);
-    }
-    
-    public static void stopEffect(int soundId){
-    	soundPlayer.stopEffect(soundId);
-    }
-    
-    public static void pauseEffect(int soundId){
-    	soundPlayer.pauseEffect(soundId);
-    }
-    
-    public static void resumeEffect(int soundId){
-    	soundPlayer.resumeEffect(soundId);
-    }
-    
-    public static float getEffectsVolume(){
-    	return soundPlayer.getEffectsVolume();
-    }
-    
-    public static void setEffectsVolume(float volume){
-    	soundPlayer.setEffectsVolume(volume);
-    }
-    
-    public static void preloadEffect(String path){
-    	soundPlayer.preloadEffect(path);
-    }
-    
-    public static void unloadEffect(String path){
-    	soundPlayer.unloadEffect(path);
-    }
-    
-    public static void stopAllEffects(){
-    	soundPlayer.stopAllEffects();
-    }
-    
-    public static void pauseAllEffects(){
-    	soundPlayer.pauseAllEffects();
-    }
-    
-    public static void resumeAllEffects(){
-    	soundPlayer.resumeAllEffects();
-    }
-    
-    public static void end(){
-    	backgroundMusicPlayer.end();
-    	soundPlayer.end();
-    }
-    
-    public static String getCocos2dxPackageName(){
-    	return packageName;
-    }
-    
-    public static void terminateProcess(){
-    	android.os.Process.killProcess(android.os.Process.myPid());
-    }
-
-    @Override
-    protected void onResume() {
-    	super.onResume();
-    	if (accelerometerEnabled) {
-    	    accelerometer.enable();
-    	}
-    }
-
-    @Override
-    protected void onPause() {
-    	super.onPause();
-    	if (accelerometerEnabled) {
-    	    accelerometer.disable();
-    	}
-    }
-
-    protected void setPackageName(String packageName) {
-    	Cocos2dxActivity.packageName = packageName;
-    	
-    	String apkFilePath = "";
-        ApplicationInfo appInfo = null;
-        PackageManager packMgmr = getApplication().getPackageManager();
-        try {
-            appInfo = packMgmr.getApplicationInfo(packageName, 0);
-        } catch (NameNotFoundException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Unable to locate assets, aborting...");
-        }
-        apkFilePath = appInfo.sourceDir;
-        Log.w("apk path", apkFilePath);
-
-        // add this link at the renderer class
-        nativeSetPaths(apkFilePath);
-    }
-    
-    private void showDialog(String title, String message){
-    	Dialog dialog = new AlertDialog.Builder(this)
-	    .setTitle(title)
-	    .setMessage(message)
-	    .setPositiveButton("Ok",
-	    new DialogInterface.OnClickListener()
-	    {
-	    	public void onClick(DialogInterface dialog, int whichButton){
-	    		
-	    	}
-	    }).create();
-
-	    dialog.show();
-    }
-}
-
-class DialogMessage {
-	public String title;
-	public String message;
+	// ===========================================================
+	// Fields
+	// ===========================================================
 	
-	public DialogMessage(String title, String message){
-		this.message = message;
-		this.title = title;
+	private Cocos2dxGLSurfaceView mGLSurefaceView;
+	private Cocos2dxHandler mHandler;
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	@Override
+	protected void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		this.init();
+
+		Cocos2dxHelper.init(this, this);
 	}
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		Cocos2dxHelper.onResume();
+		this.mGLSurefaceView.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+		Cocos2dxHelper.onPause();
+		this.mGLSurefaceView.onPause();
+	}
+
+	public void showDialog(final String pTitle, final String pMessage) {
+		Message msg = new Message();
+		msg.what = Cocos2dxHandler.HANDLER_SHOW_DIALOG;
+		msg.obj = new Cocos2dxHandler.DialogMessage(pTitle, pMessage);
+		this.mHandler.sendMessage(msg);
+	}
+
+	public void showEditTextDialog(final String pTitle, final String pContent, final int pInputMode, final int pInputFlag, final int pReturnType, final int pMaxLength) { 
+		Message msg = new Message();
+		msg.what = Cocos2dxHandler.HANDLER_SHOW_EDITBOX_DIALOG;
+		msg.obj = new Cocos2dxHandler.EditBoxMessage(pTitle, pContent, pInputMode, pInputFlag, pReturnType, pMaxLength);
+		this.mHandler.sendMessage(msg);
+	}
+	
+	public void runOnGLThread(final Runnable pRunnable) {
+		this.mGLSurefaceView.queueEvent(pRunnable);
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+	
+    public void init() {
+    	// Init handler
+    	this.mHandler = new Cocos2dxHandler(this);
+    			
+    	// FrameLayout
+        ViewGroup.LayoutParams framelayout_params =
+            new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
+                                       ViewGroup.LayoutParams.FILL_PARENT);
+        FrameLayout framelayout = new FrameLayout(this);
+        framelayout.setLayoutParams(framelayout_params);
+
+        // Cocos2dxEditText layout
+        ViewGroup.LayoutParams edittext_layout_params =
+            new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
+                                       ViewGroup.LayoutParams.WRAP_CONTENT);
+        Cocos2dxEditText edittext = new Cocos2dxEditText(this);
+        edittext.setLayoutParams(edittext_layout_params);
+
+        // ...add to FrameLayout
+        framelayout.addView(edittext);
+
+        // Cocos2dxGLSurfaceView
+        this.mGLSurefaceView = this.onCreateGLSurfaceView();
+
+        // ...add to FrameLayout
+        framelayout.addView(mGLSurefaceView);
+
+        mGLSurefaceView.setCocos2dxRenderer(new Cocos2dxRenderer());
+        mGLSurefaceView.setCocos2dxEditText(edittext);
+
+        // Set framelayout as the content view
+		setContentView(framelayout);
+    }
+    
+    public Cocos2dxGLSurfaceView onCreateGLSurfaceView() {
+    	return new Cocos2dxGLSurfaceView(this);
+    }
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
 }
