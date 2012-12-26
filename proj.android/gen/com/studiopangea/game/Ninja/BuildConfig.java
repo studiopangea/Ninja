fe653f4ee2b322656da4537ff80b87deda22a348
Gonzalo Diaz Cruz <gonzalo@studiopangea.com>
2012-12-26 01:34:28 -0300

    Android: package name changed.

--- proj.android/gen/com/studiopangea/game/Ninja/BuildConfig.java
+++ proj.android/gen/com/studiopangea/game/Ninja/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.studiopangea.game;
+package com.studiopangea.game.Ninja;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
--- proj.android/gen/com/studiopangea/game/Ninja/R.java
+++ proj.android/gen/com/studiopangea/game/Ninja/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.studiopangea.game;
+package com.studiopangea.game.Ninja;
 
 public final class R {
     public static final class attr {
